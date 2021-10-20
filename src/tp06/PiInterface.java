4d87eef8c7c0918d9100bcabc849a6d868ec70d9
Gaël Marcheville <gael.marcheville@telecom-paris.fr>
2021-10-20 16:44:11 +0200

    tp06 - corrections interfaces

--- src/tp06/PiInterface.java
+++ src/tp06/PiInterface.java
@@ -1,7 +1,7 @@
 package tp06;
 
 public interface PiInterface {
-	public PiInterface init(int n) ; //initialise une fonction pi sur n sommet valant tous +inf
+	public void init(VertexInterface[] vertices) ; //initialise une fonction pi sur les sommets valant tous +inf
 	public int value(VertexInterface s) ; //donne la valeur de pi(s)
 	public void changeValue(VertexInterface s, int x) ; //change la valeur de pi(s)
 }
